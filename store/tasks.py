e2f6b13228a540957f73826a90f8d3cb2aba12e3
BFX09 <bardia.azari@gmail.com>
2022-03-02 21:36:44 +0330

    master

--- store/tasks.py
+++ store/tasks.py
@@ -9,30 +9,6 @@
 # import os
 
 # @shared_task
-# def upload_image(data):
-    
-#     print('Uploading image...')
-    
-#     sleep(10)
-    
-#     product_id = data['product_id']
-
-#     byte_data = data['image'].encode(encoding='utf-8')
-#     b = base64.b64decode(byte_data)
-#     img = Image.open(io.BytesIO(b))
-#     img.save(data['name'], format=img.format)
-    
-#     with open(data['name'], 'rb') as file:
-#         picture = File(file)
-
-#         instance = ProductImage(product_id=product_id, image=picture)
-#         instance.save()
-    
-#     os.remove(data['name'])
-
-#     print('Uploaded!')
-
-# @shared_task
 # def upload(product_id, file_name):
 
 #     print('Uploading image...')
